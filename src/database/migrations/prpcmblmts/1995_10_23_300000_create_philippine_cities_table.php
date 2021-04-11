3c38de316ef4056223e52ef6fc00aca67ad1140c
Ronnel R. Martinez <woenel@users.noreply.github.com>
2021-04-11 09:48:59 +0800

    Update 1995_10_23_300000_create_philippine_cities_table.php

--- src/database/migrations/prpcmblmts/1995_10_23_300000_create_philippine_cities_table.php
+++ src/database/migrations/prpcmblmts/1995_10_23_300000_create_philippine_cities_table.php
@@ -15,11 +15,11 @@ class CreatePhilippineCitiesTable extends Migration
     {
         Schema::create('philippine_cities', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('psgc_code');
+            $table->string('psgc_code')->index();
             $table->string('city_municipality_description');
             $table->string('region_description');
-            $table->string('province_code');
-            $table->string('city_municipality_code');
+            $table->string('province_code')->index();
+            $table->string('city_municipality_code')->index();
             $table->timestamp('created_at')->useCurrent();
             $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
         });
