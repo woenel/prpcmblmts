4c679b71fa2c06718ffe54928d8e63e007dab86e
Ronnel R. Martinez <woenel@users.noreply.github.com>
2021-04-11 09:49:26 +0800

    Update 1995_10_23_400000_create_philippine_barangays_table.php

--- src/database/migrations/prpcmblmts/1995_10_23_400000_create_philippine_barangays_table.php
+++ src/database/migrations/prpcmblmts/1995_10_23_400000_create_philippine_barangays_table.php
@@ -15,11 +15,11 @@ class CreatePhilippineBarangaysTable extends Migration
     {
         Schema::create('philippine_barangays', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('barangay_code');
+            $table->string('barangay_code')->index();
             $table->string('barangay_description');
-            $table->string('region_code');
-            $table->string('province_code');
-            $table->string('city_municipality_code');
+            $table->string('region_code')->index();
+            $table->string('province_code')->index();
+            $table->string('city_municipality_code')->index();
             $table->timestamp('created_at')->useCurrent();
             $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
         });
